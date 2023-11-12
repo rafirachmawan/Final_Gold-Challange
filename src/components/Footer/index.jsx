bc32d31c5abacaa31ae1a55b96ae76f4988dc5f2
rafirachmawan <rafirachmawan1987@gmail.com>
2023-11-12 19:06:30 +0700

    Add Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,23 +1,38 @@
 import React from "react";
 // import logoimg from "../../assets/Logo.png";
-// import "../style.css"
+import "./style.css";
 
 const Footer = () => {
     return(
-        <div class="auto-group-ezfu-VTy" id="9ufPPBpRNwjcGfsqcUezFu">
-<div class="copyright-NXm" id="42:58">
-<img class="logo-6yZ" src={logoimg} />
-<p class="designmodo-a-SGj"
->© 2020 Epixelab. All rights reserved.</p>
-</div>
-<div class="menu-Lsu" id="42:61">
-<p class="portfolio-GFm" id="42:66">Portfolio</p>
-<p class="how-it-works-BtX" id="42:65">How it Works</p>
-<p class="pricing-vLK" id="42:64">Pricing</p>
-<p class="about-4SX" id="42:63">About</p>
-<p class="login-ntK" id="42:62">Login</p>
-</div>
-</div>
+        <div class="footer-J1w">
+            <div class="address-Apq">
+      <p class="jalan-suroyo-no-161-mayangan-kota-probolonggo-672000-WNu">Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000</p>
+      <p class="binarcarrentalgmailcom-nLR">binarcarrental@gmail.com</p>
+      <p class="item-081-233-334-808-JpZ">081-233-334-808</p>
+    </div>
+    <div class="navigation-d69">
+      <p class="our-services-Yyo">Our services</p>
+      <p class="why-us-t25">Why Us</p>
+      <p class="testimonial-pRX">Testimonial</p>
+      <p class="faq-k4H">FAQ</p>
+    </div>
+    <div class="sosmed-gid">
+      <p class="connect-with-us-QuX">Connect with us</p>
+      <div class="list-item-8Kj">
+        {/* <img class="iconfacebook-FfF" src="./assets/iconfacebook.png"/>
+        <img class="iconinstagram-BYu" src="./assets/iconinstagram.png"/>
+        <img class="icontwitter-ioj" src="./assets/icontwitter.png"/>
+        <img class="iconmail-U2D" src="./assets/iconmail.png"/>
+        <img class="icontwitch-1H3" src="./assets/icontwitch.png"/> */}
+      </div>
+    </div>
+    <div class="copyright-LaD">
+      <p class="copyright-binar-2022-VT7">Copyright Binar 2022</p>
+      <div class="rectangle-74-zPs">
+      </div>
+    </div>
+
+        </div>
     )
 }
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Footer from "../../components/Footer";
+// import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Content1 from "../../components/Content1";
 import Content2 from "../../components/Content2";
 import Content3 from "../../components/Content3";
 import Content4 from "../../components/Content4";
-
+import Footer from "../../components/Footer";
 
 const Home = () => {
     return (
@@ -15,6 +15,8 @@ const Home = () => {
             <Content2/>
             <Content3/>
             <Content4/>
+            <Footer/>
+            
         </div>
     );
 };
